342580042ddcc51e7469481a88128f49aaacb9a1
tsirlucas <tsirlucas@gmail.com>
2017-10-29 22:39:53 -0300

    Adding github webhook script

---
+++ hooks/deploy.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const createHandler = require('github-webhook-handler');
+const { exec } = require('child_process');
+
+const handler = createHandler({ path: '/webhook' });
+
+http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+        res.statusCode = 404;
+        res.end('no such location')
+    })
+}).listen(7777);
+
+handler.on('error', function (err) {
+    console.error('Error:', err.message)
+});
+
+handler.on('push', function (event) {
+    exec('git fetch && git checkout origin/master -- ./package.json', (err, stdout, stderr) => {
+        exec('yarn', (err, stdout, stderr) => {
+            exec('git pull', (err, stdout, stderr) => {
+                console.log('cb rullz im sleepy');
+            });
+        });
+    });
+});
